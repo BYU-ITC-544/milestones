90218dd3de8758693265dc93311e748e08385107
shayes20 <shayes2033@gmail.com>
2024-11-05 15:50:45 -0700

    mysql version

--- milestone-4/website/app/include/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$DB_AVAILABLE = false;
-if ($_CONFIG->database_host != 'DISABLED') {
-    $redis = new Redis(); 
-    $redis->connect($_CONFIG->database_host, 6379); 
-    $DB_AVAILABLE = true;
-}
-
-?>
--- milestone-4/website/app/posts/example.php
+++
@@ -1,7 +0,0 @@
-<?php $PAGE='Example';$SUBTITLE='This is Cool!';$LASTEDIT='2023-04-12T02:06:17+0000';$AUTHOR='test';include("../include/postheader.php");?>
-
-This is an example post.
-
-<?php 
-echo strtoupper("We can do PHP here!");
-?>
